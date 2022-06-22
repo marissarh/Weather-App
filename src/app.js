f6ee52e28e4dc5876aabf0a8347f36e49ccd0001
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2022-06-22 15:32:15 -0700

    Added Search Engine

--- src/app.js
+++ src/app.js
@@ -42,7 +42,17 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0], description);
 }
-let apiKey = "b1ada7c4514578c6bdeff83b05cf9417";
-let city = "Paris";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+
+function search(city) {
+  let apiKey = "b1ada7c4514578c6bdeff83b05cf9417";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventionDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("New York");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
